c72962744f91a5ac1bd58bc646fe3981fc31b659
Muhammad Shamim Shoaib <shamim.akhonzada@gmail.com>
2025-02-01 00:31:49 +0500

    added Store

--- lib/features/shop/screens/store.dart
+++ lib/features/shop/screens/store.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:skardu_app/common/widgets/custom_shapes/containers/rounded_contianer.dart';
+import 'package:skardu_app/common/widgets/appbar/tab_bar.dart';
 import 'package:skardu_app/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:skardu_app/common/widgets/layouts/grid_layout.dart';
 import 'package:skardu_app/common/widgets/text/section_heading.dart';
-import 'package:skardu_app/utils/constants/image_strings.dart';
+import 'package:skardu_app/common/widgets/brand/skd_brand_card.dart';
 import 'package:skardu_app/utils/constants/sizes.dart';
 
 import '../../../common/product_cart/cart_menu_icon.dart';
 import '../../../common/widgets/appbar/appbar.dart';
-import '../../../common/widgets/images/skd_circular_image.dart';
-import '../../../common/widgets/text/skd_brand_title_with_verified_icon.dart';
+import '../../../common/widgets/brand/brand_show_case.dart';
+import '../../../common/widgets/brand/category_tab.dart';
 import '../../../utils/constants/color.dart';
-import '../../../utils/constants/enums.dart';
+import '../../../utils/constants/image_strings.dart';
 import '../../../utils/helpers/helper_functions.dart';
 
 class StoreScreen extends StatelessWidget {
@@ -20,104 +20,77 @@ class StoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = SkdHelperFunction.isDarkMode(context);
-    return Scaffold(
-      appBar: SkdAppBar(
-        title: Text(
-          'Store',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-        actions: [
-          SkdCartCounterIcon(
-            onPressed: () {},
-            iconColor: dark ? SkdColors.white : SkdColors.black,
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: SkdAppBar(
+          title: Text(
+            'Store',
+            style: Theme.of(context).textTheme.headlineMedium,
           ),
-        ],
-      ),
-      body: NestedScrollView(
-        headerSliverBuilder: (_, innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              automaticallyImplyLeading: false,
-              pinned: true,
-              floating: true,
-              backgroundColor: dark ? SkdColors.black : SkdColors.white,
-              expandedHeight: 440,
-              flexibleSpace: Padding(
-                padding: EdgeInsets.all(SkdSizes.defaultSpace),
-                child: ListView(
-                  physics: NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  children: [
-                    ///Search Bar
-                    SizedBox(height: SkdSizes.spaceBtwItems),
-                    SkdSearchContainer(
-                      text: "Search in Store",
-                      showBackground: false,
-                      padding: EdgeInsets.zero,
+          actions: [
+            SkdCartCounterIcon(
+              onPressed: () {},
+              iconColor: dark ? SkdColors.white : SkdColors.black,
+            ),
+          ],
+        ),
+        body: NestedScrollView(
+            headerSliverBuilder: (_, innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  pinned: true,
+                  floating: true,
+                  backgroundColor: dark ? SkdColors.black : SkdColors.white,
+                  expandedHeight: 440,
+                  flexibleSpace: Padding(
+                    padding: EdgeInsets.all(SkdSizes.defaultSpace),
+                    child: ListView(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      children: [
+                        ///Search Bar
+                        SizedBox(height: SkdSizes.spaceBtwItems),
+                        SkdSearchContainer(
+                          text: "Search in Store",
+                          showBackground: false,
+                          padding: EdgeInsets.zero,
+                        ),
+                        SizedBox(height: SkdSizes.spaceBtwSections),
+
+                        /// --Featured Brands
+                        SkdSectionHeading(
+                            title: "Featured Brands", onPressed: () {}),
+                        SizedBox(height: SkdSizes.spaceBtwItems / 1.5),
+                        SkdGridLayout(
+                            itemCount: 4,
+                            mainAxisExtent: 80,
+                            itemBuilder: (_, index) {
+                              return SkdBrandCard(showBorder: true);
+                            })
+                      ],
                     ),
-                    SizedBox(height: SkdSizes.spaceBtwSections),
+                  ),
 
-                    /// --Featured Brands
-                    SkdSectionHeading(
-                        title: "Featured Brands", onPressed: () {}),
-                    SizedBox(height: SkdSizes.spaceBtwItems / 1.5),
-                    SkdGridLayout(
-                        itemCount: 4,
-                        mainAxisExtent: 80,
-                        itemBuilder: (_, index) {
-                          return GestureDetector(
-                            onTap: () {},
-                            child: SkdRoundedContainer(
-                              padding: EdgeInsets.all(SkdSizes.sm),
-                              showBorder: true,
-                              backgroundColor: Colors.transparent,
-                              child: Row(
-                                children: [
-                                  ///Icon
-                                  Flexible(
-                                    child: SkdCircularImage(
-                                      image: SkdImage.clothIcon,
-                                      backgroundColor: Colors.transparent,
-                                      overlayColor: dark
-                                          ? SkdColors.white
-                                          : SkdColors.black,
-                                    ),
-                                  ),
-                                  SizedBox(width: SkdSizes.spaceBtwItems / 2),
-
-                                  ///Text
-                                  Expanded(
-                                    child: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        SkdBrandTitleWithVerifiedIcon(
-                                          title: 'Nike',
-                                          brandTextSize: TextSizes.large,
-                                        ),
-                                        Text("256 Products",
-                                            overflow: TextOverflow.ellipsis,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .labelMedium)
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          );
-                        })
-                  ],
-                ),
-              ),
-            ),
-          ];
-        },
-        body: Container(
-            // Add your body content here
-            ),
+                  ///Tab
+                  bottom: SkdTabBar(tabs: [
+                    Tab(child: Text('Sports')),
+                    Tab(child: Text('Furniture')),
+                    Tab(child: Text('Electronics')),
+                    Tab(child: Text('Clothes')),
+                    Tab(child: Text('Cosmetics')),
+                  ]),
+                )
+              ];
+            },
+            body: TabBarView(children: [
+             SkdCategoryTab(),
+              SkdCategoryTab(),
+              SkdCategoryTab(),
+              SkdCategoryTab(),
+              SkdCategoryTab()
+            ])),
       ),
     );
   }
